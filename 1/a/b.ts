525e35afd21ca090438cec973559b36220db422b
Steho <s1710629025@students.fh-hagenberg.at>
2022-12-06 19:42:51 +0100

    Solve 1-b puzzle

---
+++ 1/a/b.ts
@@ -0,0 +1,17 @@
+const input = await Deno.readTextFile("input.txt");
+
+const caloriesList: string[] = input.split(/\r\n/);
+
+let currentElfsCalories = 0;
+const fatElves: number[] = [0,0,0];
+
+caloriesList.forEach((calories) => {
+    if (!calories) currentElfsCalories = 0;
+    currentElfsCalories+=Number(calories);
+    if (currentElfsCalories>fatElves[2]) fatElves[2] = currentElfsCalories;
+    if (fatElves[2]>fatElves[1]) [fatElves[1], fatElves[2]] = [fatElves[2], fatElves[1]];
+    if (fatElves[1]>fatElves[0]) [fatElves[0], fatElves[1]] = [fatElves[1], fatElves[0]];
+});
+
+console.log(fatElves.reduce((prev, curr) => prev + curr, 0));
+
