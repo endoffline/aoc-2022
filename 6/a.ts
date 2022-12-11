ca54290aa86fe8788b7f7f2f3b7aa75ff9ce1f48
endoffline <stefan.hoeller93@gmail.com>
2022-12-11 01:03:09 +0100

    Solve 6-a-b puzzle

---
+++ 6/a.ts
@@ -0,0 +1,13 @@
+const stream = await Deno.readTextFile('input.txt');
+
+let startOfPacket = 0;
+for (let i = 4; i <= stream.length; i++) {
+
+    const window = new Set(stream.substring(i - 4, i).split(''));
+    if (window.size === 4) {
+        startOfPacket = i;
+        break;
+    } 
+}
+
+console.log(startOfPacket);
---
+++ 6/b.ts
@@ -0,0 +1,17 @@
+const stream = await Deno.readTextFile('input.txt');
+
+const sequenceSize = 14;
+let startOfSequence = 0, i = sequenceSize;
+
+while (!startOfSequence && i <= stream.length) {
+    const window = new Set(stream.substring(i - sequenceSize, i).split(''));
+
+    if (window.size === sequenceSize) {
+        startOfSequence = i;
+        break;
+    }
+
+    i++;
+}
+
+console.log(startOfSequence);
